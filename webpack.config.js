4012c242b369155fc569bb1f009835a55ac6ab89
kamijin_fanta <kamijin@live.jp>
2015-09-03 05:36:22 +0900

    プレイヤー情報を表示するチップを追加。背景色などを変更。

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,14 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel?optional[]=runtime&stage=0'
             },
-            { test: /\.json$/, loader: "json-loader"}
+            { test: /\.json$/, loader: "json-loader"},
+            {
+                test: /.*\.(gif|png|jpe?g|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+                ]
+            }
         ]
     },
     plugins: [
