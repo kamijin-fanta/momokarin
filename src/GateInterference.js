7f239c412a5ff6dee9a2f9560fd50eb3c9b5eec5
kamijin_fanta <kamijin@live.jp>
2015-09-04 19:24:46 +0900

    PlayerMakerに反応させるやつ。コミット忘れてへぺろ(・ω<)

---
+++ src/GateInterference.js
@@ -0,0 +1,51 @@
+import InterferenceBase from "./InterferenceBase";
+import Vector2 from "./Vector2";
+
+export default class extends InterferenceBase {
+    constructor(field, players) {
+        super();
+        this.fieldElement = field;
+        this.players = players;
+    }
+    init(vectorObj){
+    }
+
+    beforeTouchStart(vectorObj, e) {
+    }
+    afterTouchStart(vectorObj, e) {
+    }
+
+    beforeTouchMove(vectorObj, e) {
+    }
+    afterTouchMove(vectorObj, e) {
+    }
+
+    beforeTouchEnd(vectorObj, e) {
+    }
+    afterTouchEnd(vectorObj, e) {
+    }
+
+    beforeRender(vectorObj){
+        // PlayerMakerに反応させる
+        let pos = vectorObj.getCenterPosition();
+        let match;
+        this.players.each((i,v) => {
+            let vect = Vector2;
+            let rectLT = new Vector2(v.offsetLeft, v.offsetTop);
+            let rectRB = rectLT.clone().add(new Vector2(v.offsetWidth, v.offsetHeight));
+            let flag = Vector2.contain(rectLT, rectRB, pos, v.offsetWidth);
+
+            if(flag){
+                match = v;
+                return false;
+            }
+        });
+        if(vectorObj.matchElement != match){
+            // todo テストでクラス名の表示を行っているので置き換えする
+            vectorObj.element.text(match?match.className:"");
+            vectorObj.matchElement = match;
+        }
+    }
+    afterRender(vectorObj){
+    }
+}
